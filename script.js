f7dd06bf448020ff31ec440d723a1bd0c53b513c
Umarali <umaraliomorov13@gmail.com>
2024-10-07 00:35:27 +0600

    master

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ document.getElementById('checkButton').addEventListener('click', function() {
     const message = document.getElementById('message');
 
     if (input === 'cas') {
-        window.location.href = '../page-two/secret.html'; // Укажите здесь нужную страницу
+        window.location.href = './secret.html'; // Укажите здесь нужную страницу
     }
     if (input === 'лапочка') {
-        window.location.href = '../page-two/lapochka.html';
+        window.location.href = './lapochka.html';
     }
     if (input === 'фенёк') {
-        window.location.href = '../page-two/phenek.html';
+        window.location.href = './phenek.html';
     }
     if (input === 'фенек') {
-        window.location.href = '../page-two/phenek.html';
+        window.location.href = './phenek.html';
     }
      else {
         message.textContent = 'не то слово,я в тебя верю';
